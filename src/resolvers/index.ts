9a29db571703a414a192f3adc79fc0fd3adbd056
Anartz Mugika Ledo <mugan86@gmail.com>
2021-06-03 20:33:35 +0200

    Resolvers OK

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -2,11 +2,8 @@ import path from "path";
 import { mergeResolvers } from "@graphql-tools/merge";
 import { loadFilesSync } from "@graphql-tools/load-files";
 
-// console.log(path.join(__dirname, './**/*.resolver.ts'));
-const resolversFilesExtension =
-  process.env.NODE_ENV !== "production" ? "./**/*.ts" : "./**/*.js";
-const resolversArray = loadFilesSync(path.join(__dirname, resolversFilesExtension));
-// console.log(resolversArray);
+const resolversArray = loadFilesSync(path.join(__dirname, './**'));
+console.log(resolversArray);
 const resolvers = mergeResolvers(resolversArray);
 
 export default resolvers;
