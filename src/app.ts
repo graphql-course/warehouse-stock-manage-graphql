f5d65df8034217103b28e94763a075e2166169c0
Anartz Mugika Ledo <mugan86@gmail.com>
2021-06-05 20:29:16 +0200

    Refactor app options

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import Server from "./server";
 
 import schema from "./schema";
-const server = new Server(schema);
-server.listen((port: number) =>
+
+new Server(schema).listen((port: number) =>
   console.log(`http://localhost:${port}/graphql`)
 );
